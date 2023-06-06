c1315e540baff985e72445572e29eaaf775cf653
Jyoungjo <zimbote0411@gmail.com>
2023-06-07 01:04:31 +0900

    feat: 백준 1436 영화감독 숌 - BaekJoon1436 추가

---
+++ src/main/java/com/example/baekjoon/bruteforce/BaekJoon1436.java
@@ -0,0 +1,24 @@
+package com.example.baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int cnt = 1;
+
+        while (cnt != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
+            }
+        }
+
+        System.out.print(num);
+    }
+}
