3d9decc98a9ec38af2ebc3119eeeab80a95599dd
Jyoungjo <zimbote0411@gmail.com>
2023-06-22 02:01:12 +0900

    feat: 백준 17219 비밀번호 찾기 - BaekJoon17219 추가

---
+++ src/main/java/com/example/BaekJoon17219.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BaekJoon17219 {
+    // 백준 17219 비밀번호 찾기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            String key = br.readLine();
+            if (map.containsKey(key)) sb.append(map.get(key)).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
