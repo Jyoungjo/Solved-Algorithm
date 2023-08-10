e0b76104208cb75d9ccb6c4fc4fa278730d853cc
Jyoungjo <zimbote0411@gmail.com>
2023-08-11 00:52:37 +0900

    feat: 프로그래머스 42578 의상 - Programmers42578 추가

---
+++ src/main/java/com/example/programmers/Programmers42578.java
@@ -0,0 +1,21 @@
+package com.example.programmers;
+
+import java.util.HashMap;
+
+public class Programmers42578 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (String[] cloth : clothes) {
+            hashMap.put(cloth[1], hashMap.getOrDefault(cloth[1], 0) + 1);
+        }
+
+        for (String key : hashMap.keySet()) {
+            answer *= hashMap.get(key) + 1;
+        }
+
+        return answer - 1;
+    }
+}
